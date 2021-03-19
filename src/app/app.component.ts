853952dd0455290b50d83ef7b926e60a1c970a57
escleur <escleur@gmail.com>
2021-03-18 21:59:46 -0300

    ej1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-proyecto';
+  edadUno:number = 0;
+  edadDos:number = 0;
+  suma:number = 0;
+  promedio:number = 0;
+
+  
+  Calcular(){
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+  Reset(){
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.suma = 0;
+    this.promedio = 0;
+
+  }
 }
